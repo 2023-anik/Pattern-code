4ee413f8ada69e773206c5f4428bf9a275ba86ef
Anik Sen <cse2023anik@gmail.com>
2024-03-21 04:47:50 +0600

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=0;
+    while(i<n){
+        int j=0;
+        while(j<n){
+            cout<<j+1<<" ";
+            j++;
+        }i++;
+        cout<<endl;
+    }
+}
