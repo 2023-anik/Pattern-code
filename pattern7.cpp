5e5ff3e6a91a7247bf73847dd07e4bb2647bedec
Anik Sen <cse2023anik@gmail.com>
2024-03-21 13:29:56 +0600

    Add files via upload

---
+++ pattern7.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=0;
+    while(i<n){
+        int j=0;
+        while(j<=i){
+            cout<<"* ";
+            j++;
+        }i++;
+        cout<<endl;
+    }return 0;
+}
