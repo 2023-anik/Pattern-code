d5d3dd3464c80683baabc40b95f243748d604f45
Anik Sen <cse2023anik@gmail.com>
2024-03-21 13:19:45 +0600

    Add files via upload

---
+++ pattern6.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=0;
+    int k=n*n;
+    while(i<n){
+        int j=0;
+        while(j<n){
+            cout<<k--<<" ";
+            j++;
+        }i++;
+        cout<<endl;
+    }return 0;
+}
