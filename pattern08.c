addda7f803b652180f7166a819d73ff1a098ad63
Anik Sen <cse2023anik@gmail.com>
2024-04-29 04:04:59 +0600

    pattern08.c

---
+++ pattern08.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int space=n-1;
+    int star=1;
+    for(int i=1; i<=(2*n)-1; ++i){
+        for(int j=1; j<=space; j++)
+            printf("  ");
+        for(int j=1; j<=star; j++)
+            printf("* ");
+        if(i<n){
+            space--;
+            star+=2;
+        }
+        else{
+            space++;
+            star-=2;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+// intput: 5
+// output:
+//         * 
+//       * * * 
+//     * * * * * 
+//   * * * * * * * 
+// * * * * * * * * * 
+//   * * * * * * * 
+//     * * * * * 
+//       * * * 
+//         * 
